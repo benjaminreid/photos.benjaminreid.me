5b906ca441d8ad54f782ba90c30e9afce537bcf1
Benjamin Reid <benjamin@nouveller.com>
2016-06-05 16:51:42 +0100

    Index route and views

--- app/main.js
+++ app/main.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 import store from './store';
-import Hello from './components/hello';
+import App from './components/app';
+import Index from './views/index';
 
 const history = syncHistoryWithStore(browserHistory, store);
 
 render(
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={Hello}/>
+      <Route path="/" component={App}>
+        <IndexRoute component={Index}/>
+      </Route>
     </Router>
   </Provider>,
   document.querySelector('#react-root')
