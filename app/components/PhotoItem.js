459b5df12d0dd2907b9abaaf0e2cfbb015054d43
Benjamin Reid <benjamin@nouveller.com>
2016-06-07 15:19:59 +0100

    Link to the lighbox

--- app/components/PhotoItem.js
+++ app/components/PhotoItem.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router';
 import classNames from 'classnames';
 import styles from 'styles/components/photo-item';
 
-function PhotoItem({ url }) {
+function PhotoItem({ link, url }) {
   let base = classNames(styles.base);
+  var image;
+
+  if (link) {
+    image = <Link to={link}><img src={url}/></Link>;
+  } else {
+    image = <img src={url}/>;
+  }
 
   return (
     <div className={base}>
-      <img src={url}/>
+      {image}
     </div>
   );
 }
